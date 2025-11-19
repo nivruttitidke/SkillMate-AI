351658b762121ff4ab276456454059a35825d5f7
Nivrutti Tidke <nivrutitidke108@gmail.com>
2025-11-19 14:34:27 +0530

    Updated API integration + final UI fixes

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://skillmate-backend-5g3j.onrender.com", 
+  baseURL: import.meta.env.VITE_API_URL, 
 });
 
 export default api;
